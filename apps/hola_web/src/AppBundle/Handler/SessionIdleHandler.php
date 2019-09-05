409e901209a760a6f8a0eede8120c2465962cca2
Ivan <peraltaalvarez.ivan@gmail.com>
2019-09-05 18:47:38 +0200

    Logout 5 minutes

---
+++ apps/hola_web/src/AppBundle/Handler/SessionIdleHandler.php
@@ -0,0 +1,47 @@
+<?php
+// AppBundle/Handler/SessionIdleHandler.php
+namespace AppBundle\Handler;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+class SessionIdleHandler
+{
+    protected $session;
+    protected $tokenStorage;
+    protected $router;
+    protected $maxIdleTime;
+    public function __construct(
+        SessionInterface $session,
+        TokenStorageInterface $tokenStorage,
+        RouterInterface $router,
+        $maxIdleTime = 0
+    ) {
+        $this->session = $session;
+        $this->tokenStorage = $tokenStorage;
+        $this->router = $router;
+        $this->maxIdleTime = $maxIdleTime;
+    }
+    
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()
+            || $this->maxIdleTime <= 0) {
+            return;
+        }
+
+        $session = $this->session;
+        $session->start();
+
+        if ((time() - $session->getMetadataBag()->getLastUsed()) <= $this->maxIdleTime) {
+            return;
+        }
+
+        $this->tokenStorage->setToken();
+        $session->getFlashBag()->set('info', 'You have been logged out due to inactivity.');
+
+        $event->setResponse(new RedirectResponse($this->router->generate('app_login')));
+    }
+}
